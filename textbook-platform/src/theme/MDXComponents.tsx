b2813a0a8634a951236fed7c83e32c4db9822876
zareenarif <165668465+zareenarif@users.noreply.github.com>
2025-12-17 23:31:06 +0500

    Fix global MDX component registration for SSG

--- textbook-platform/src/theme/MDXComponents.tsx
+++ textbook-platform/src/theme/MDXComponents.tsx
@@ -1,14 +1,39 @@
+// import React from 'react';
+// import MDXComponents from '@theme-original/MDXComponents';
+// import Header from '../components/Header';
+// import ChapterCard from '../components/ChapterCard';
+// import CodeBlock from '../components/CodeBlock';
+// import DiagramContainer from '../components/DiagramContainer';
+// import ExerciseSection from '../components/ExerciseSection';
+// import QuizComponent from '../components/QuizComponent';
+// import GlossaryTerm from '../components/GlossaryTerm';
+// import LessonLayout from '../components/LessonLayout';
+// import Sidebar from '../components/Sidebar';
+
+// export default {
+//   ...MDXComponents,
+//   Header,
+//   ChapterCard,
+//   CodeBlock,
+//   DiagramContainer,
+//   ExerciseSection,
+//   QuizComponent,
+//   GlossaryTerm,
+//   LessonLayout,
+//   Sidebar,
+// };
 import React from 'react';
-import MDXComponents from '@theme-original/MDXComponents';
-import Header from '../components/Header';
-import ChapterCard from '../components/ChapterCard';
-import CodeBlock from '../components/CodeBlock';
-import DiagramContainer from '../components/DiagramContainer';
-import ExerciseSection from '../components/ExerciseSection';
-import QuizComponent from '../components/QuizComponent';
-import GlossaryTerm from '../components/GlossaryTerm';
-import LessonLayout from '../components/LessonLayout';
-import Sidebar from '../components/Sidebar';
+import MDXComponents from '@theme/MDXComponents';
+
+import Header from '@site/src/components/Header';
+import ChapterCard from '@site/src/components/ChapterCard';
+import CodeBlock from '@site/src/components/CodeBlock';
+import DiagramContainer from '@site/src/components/DiagramContainer';
+import ExerciseSection from '@site/src/components/ExerciseSection';
+import QuizComponent from '@site/src/components/QuizComponent';
+import GlossaryTerm from '@site/src/components/GlossaryTerm';
+import LessonLayout from '@site/src/components/LessonLayout';
+import Sidebar from '@site/src/components/Sidebar';
 
 export default {
   ...MDXComponents,
